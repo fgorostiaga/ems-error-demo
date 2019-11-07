e351840001e0ab66554d2d621c754838c467df65
Román Castellarin <romancastellarin@gmail.com>
2019-11-07 17:17:02 +0100

    minor changes

--- sut/__main__.py
+++ sut/__main__.py
@@ -6,6 +6,8 @@ ems = os.environ["ET_EMS_LSBEATS_HOST"]
 appid = os.environ["APPID"]
 hostport = 'http://' + ems + ":8181"
 
+print appid
+
 for _ in range(60):
     json_message = {'APPID': int(appid)}
     requests.post(self.hostport, json=json_message)
