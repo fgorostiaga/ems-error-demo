8d7f83becd5bd3b65de5992e746cc714651a7036
Román Castellarin <romancastellarin@gmail.com>
2019-11-07 17:00:21 +0100

    started demo

---
+++ sut/__init__.py
@@ -0,0 +1,9 @@
+"""
+TODO: Add description here
+"""
+
+__version__ = "0.1"
+__description__ = "EMS error demo"
+__author_name__ = ""
+__author_mail__ = ""
+__requires__ = []
---
+++ sut/__main__.py
@@ -0,0 +1,14 @@
+import os
+import requests
+from time import sleep
+
+ems = os.environ["ET_EMS_LSBEATS_HOST"]
+appid = os.environ["APPID"]
+hostport = 'http://' + ems + ":8181"
+
+for _ in range(60):
+    json_message = {'APPID': int(appid)}
+    requests.post(self.hostport, json=json_message)
+    sleep(1)
+
+
