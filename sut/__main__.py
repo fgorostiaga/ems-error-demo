3c5f7655d52f83df7072d43e8068c74b4bbf0f6a
Román Castellarin <romancastellarin@gmail.com>
2019-11-14 13:40:49 +0100

    increased test time to 2 minutes and added message number

--- sut/__main__.py
+++ sut/__main__.py
@@ -9,13 +9,13 @@ hostport = 'http://' + ems + ":8181"
 
 print "App ID", appid
 
-for i in range(60):
-    json_message = {'APPID': int(appid)}
+for i in range(240):
+    json_message = {'APPID': int(appid), 'NUMBER': i}
     r = requests.post(hostport, json=json_message)
     print r.content
     print "sent message n", i, "on app", appid
     sys.stdout.flush()
-    sleep(1)
+    sleep(0.5)
 
 json_message = {'STOP': True}
 r = requests.post(hostport, json=json_message)
