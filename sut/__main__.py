4b7ababd49a6fbf4634162d03c995ab231452038
Felipe Gorostiaga <feligorostiaga@gmail.com>
2019-11-15 13:32:13 -0300

    tests

--- sut/__main__.py
+++ sut/__main__.py
@@ -2,17 +2,20 @@ import os
 import requests
 from time import sleep
 import sys
+import socket
 
 ems = os.environ["ET_EMS_LSBEATS_HOST"]
 appid = os.environ["APPID"]
 # TODO: use ET_EMS_HTTPINEVENTS_API
 hostport = 'http://' + ems + ":8181"
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
 
 print "App ID", appid
 print "EMS url", hostport
 
 for i in range(240):
-    json_message = {'APPID': int(appid), 'NUMBER': i}
+    json_message = {'APPID': int(appid), 'NUMBER': i, "SUTIP": IPAddr}
     r = requests.post(hostport, json=json_message)
     print r.content
     print "sent message n", i, "on app", appid
